71c50340013a16c37cd597c58fb32447a2f9f47c
shawlycode <kutinaorlando@gmail.com>
2024-07-02 15:56:58 +0000

    updated

--- models/eventModels.js
+++ models/eventModels.js
@@ -1,14 +1,14 @@
-import { model, Schema,Types, } from "mongoose";
+import { model, Schema, Types, } from "mongoose";
 
-import {toJSON} from "@reis/mongoose-to-json";
+import { toJSON } from "@reis/mongoose-to-json";
 
 const eventSchema = new Schema({
     name: { type: String, unique: true, required: true },
-    date: { type:Date},
-    price: {type:Number, required: true },
+    date: { type: Date },
+    price: { type: Number, required: true },
     location: { type: String, required: true },
-    
-    image:{ type: String, required: true},
+
+    image: { type: String, required: true },
 }, {
     timestamps: true
 });
