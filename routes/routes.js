dcea717d53acb0579efeecb8993b1f1bf0e182ea
shawlycode <kutinaorlando@gmail.com>
2024-07-04 18:58:59 +0000

    added multer and and connected db

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,14 @@
 import { Router } from "express";
-import { addEvent, getEvent, getEvents, updateEvent } from "../controllers/eventControllers.js";
+import { addEvent, getEvent, getEvents, updateEvents } from "../controllers/eventControllers.js";
+import { remoteUploads } from "../middleware/uploads.js";
 
 //create Router
 const eventRouter = Router();
 
 //create all routes
 eventRouter.get('/events', getEvents);
-eventRouter.get('/event/:id', getEvent)
-eventRouter.post('/event', addEvent);
-eventRouter.patch('/event/:id', updateEvent);
-
-
-
+eventRouter.get('/events/:id', getEvent)
+eventRouter.post('/events', remoteUploads.single('image'), addEvent);
+eventRouter.patch('/events/:id', remoteUploads.single('image'), updateEvents);
 
 export default eventRouter;
